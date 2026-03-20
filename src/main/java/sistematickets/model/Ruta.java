847bb60d81cfc4a0910196357edaebd7f3a2aa56
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 11:17:44 -0500

    Feat: encapsulamiento de la clase Ruta y creacion de sus metodos

--- src/main/java/sistematickets/model/Ruta.java
+++ src/main/java/sistematickets/model/Ruta.java
@@ -14,4 +14,67 @@ public class Ruta implements InterfazImprimible {
     private String ciuDestino;
     private int kilometros;
     private int tiemReco;
+    
+    public Ruta(String codRuta, String ciuOrigen, String ciuDestino, int kilometros, int tiemReco) {
+        this.codRuta = codRuta;
+        this.ciuOrigen = ciuOrigen;
+        this.ciuDestino = ciuDestino;
+        this.kilometros = kilometros;
+        this.tiemReco = tiemReco;
+    }
+
+    public String getCodRuta() {
+        return codRuta;
+    }
+
+    public void setCodRuta(String codRuta) {
+        this.codRuta = codRuta;
+    }
+
+    public String getCiuOrigen() {
+        return ciuOrigen;
+    }
+
+    public void setCiuOrigen(String ciuOrigen) {
+        this.ciuOrigen = ciuOrigen;
+    }
+
+    public String getCiuDestino() {
+        return ciuDestino;
+    }
+
+    public void setCiuDestino(String ciuDestino) {
+        this.ciuDestino = ciuDestino;
+    }
+
+    public int getKilometros() {
+        return kilometros;
+    }
+
+    public void setKilometros(int kilometros) {
+        this.kilometros = kilometros;
+    }
+
+    public int getTiemReco() {
+        return tiemReco;
+    }
+
+    public void setTiemReco(int tiemReco) {
+        this.tiemReco = tiemReco;
+    }
+    
+    @Override
+    public String imprimirDetalle(){
+        return "Código de ruta  : " + codRuta + "\n" +
+               "Ciudad origen   : " + ciuOrigen + "\n" +
+               "Ciudad destino  : " + ciuDestino + "\n" +
+               "Kilómetros      : " + kilometros + " km\n" +
+               "Tiempo recorrido: " + tiemReco + " min";
+    }
+
+    @Override
+    public String toFile() {
+        return  codRuta + ";" + ciuOrigen + ";" + ciuDestino + ";" + kilometros + ";" + tiemReco;
+    }
+    
 }
