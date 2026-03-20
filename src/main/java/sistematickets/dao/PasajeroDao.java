4fa42e84bff95978e7a1ac573a11f0b4fe2bd097
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-20 14:18:25 -0500

    feat: creacion de los metodos guardarPasajeros(), cargarPasajeros() en PasajeroDao

--- src/main/java/sistematickets/dao/PasajeroDao.java
+++ src/main/java/sistematickets/dao/PasajeroDao.java
@@ -4,10 +4,63 @@
  */
 package sistematickets.dao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import sistematickets.model.Pasajero;
+import sistematickets.model.PasajeroAdultoMayor;
+import sistematickets.model.PasajeroEstudiante;
+import sistematickets.model.PasajeroRegular;
+import sistematickets.util.RutaArchivos;
+
 /**
  *
  * @author jadis
  */
 public class PasajeroDao {
+    
+    public void guardarPasajeros(HashMap<String, Pasajero> pasajeros) throws IOException {
+        File archivo = new File(RutaArchivos.PASAJEROS);
+        if (!archivo.exists()) {
+            archivo.createNewFile();
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo))) {
+            for (Pasajero p : pasajeros.values()) {
+                bw.write(p.toFile());
+                bw.newLine();
+            }
+        }
+    }
+
+    public HashMap<String, Pasajero> cargarPasajeros() throws IOException {
+        HashMap<String, Pasajero> pasajeros = new HashMap<>();
+        File archivo = new File(RutaArchivos.PASAJEROS);
+        if (!archivo.exists()) {
+            return pasajeros;
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] p = linea.split(";");
+                String cedula = p[0];
+                String nombre = p[1];
+                LocalDate fechaNacimiento = LocalDate.parse(p[2]);
+                String tipo = p[3];
+
+                Pasajero pasajero = switch (tipo) {
+                    case "Estudiante"   -> new PasajeroEstudiante(cedula, nombre, fechaNacimiento);
+                    case "Adulto Mayor" -> new PasajeroAdultoMayor(cedula, nombre, fechaNacimiento);
+                    default             -> new PasajeroRegular(cedula, nombre, fechaNacimiento);
+                };
+                pasajeros.put(cedula, pasajero);
+            }
+        }
+        return pasajeros;
+    }
     
 }
