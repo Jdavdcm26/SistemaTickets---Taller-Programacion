ac1469760bc2ac1472704f13733d845315c8a1c9
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-20 16:31:52 -0500

    feat: creacion de lista festivos en la clase TicketService

--- src/main/java/sistematickets/service/TicketService.java
+++ src/main/java/sistematickets/service/TicketService.java
@@ -4,10 +4,34 @@
  */
 package sistematickets.service;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import sistematickets.dao.ConductorDao;
+import sistematickets.dao.PasajeroDao;
+import sistematickets.dao.RutaDao;
+import sistematickets.dao.TicketDao;
+import sistematickets.dao.VehiculoDao;
+
 /**
  *
  * @author jadis
  */
 public class TicketService {
+    
+    private TicketDao ticketDao       = new TicketDao();
+    private PasajeroDao pasajeroDao   = new PasajeroDao();
+    private VehiculoDao vehiculoDao   = new VehiculoDao();
+    private RutaDao rutaDao           = new RutaDao();
+    private ConductorDao conductorDao = new ConductorDao();
+
+    // Lista de festivos configurada
+    private static final List<LocalDate> FESTIVOS = Arrays.asList(
+        LocalDate.of(LocalDate.now().getYear(), 1,  1),  // Año nuevo
+        LocalDate.of(LocalDate.now().getYear(), 5,  1),  // Día del trabajo
+        LocalDate.of(LocalDate.now().getYear(), 7,  20), // Independencia
+        LocalDate.of(LocalDate.now().getYear(), 8,  7),  // Batalla de Boyacá
+        LocalDate.of(LocalDate.now().getYear(), 12, 25)  // Navidad
+    );
     
 }
