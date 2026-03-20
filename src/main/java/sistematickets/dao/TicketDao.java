7105c37e4b55b8fa7f1adf8f15cefd40cd2f58cc
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-20 16:21:24 -0500

    feat: creacion de los metodos agregarTicket(), cargarTickets() en TicketDao

--- src/main/java/sistematickets/dao/TicketDao.java
+++ src/main/java/sistematickets/dao/TicketDao.java
@@ -4,10 +4,57 @@
  */
 package sistematickets.dao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import sistematickets.model.Pasajero;
+import sistematickets.model.Ticket;
+import sistematickets.model.Vehiculo;
+import sistematickets.util.RutaArchivos;
+
 /**
  *
  * @author jadis
  */
 public class TicketDao {
+    
+    public void agregarTicket(Ticket t) throws IOException {
+        File archivo = new File(RutaArchivos.TICKETS);
+        if (!archivo.exists()) archivo.createNewFile();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))) {
+            bw.write(t.toFile());
+            bw.newLine();
+        }
+    }
+
+    public HashMap<String, Ticket> cargarTickets(PasajeroDao pasajeroDao, VehiculoDao vehiculoDao,
+                                                  RutaDao rutaDao, ConductorDao conductorDao) throws IOException {
+        HashMap<String, Ticket> tickets = new HashMap<>();
+        File archivo = new File(RutaArchivos.TICKETS);
+        if (!archivo.exists()) return tickets;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] p = linea.split(";");
+                // p[0]=numeroTicket, p[1]=cedulaPasajero, p[2]=placaVehiculo
+                // p[3]=fechaCompra,  p[4]=valorFinal
+                Pasajero pasajero = pasajeroDao.buscarPorCedula(p[1]);
+                Vehiculo vehiculo = vehiculoDao.buscarPorPlaca(p[2], rutaDao, conductorDao);
+                LocalDate fechaCompra = LocalDate.parse(p[3]);
+                double valorFinal = Double.parseDouble(p[4]); // CAMBIO: lee el valor guardado
+
+                // CAMBIO: pasa valorFinal directamente, no recalcula
+                Ticket t = new Ticket(p[0], pasajero, vehiculo, fechaCompra, valorFinal);
+                tickets.put(t.getNumeroTicket(), t);
+            }
+        }
+        return tickets;
+    }
     
 }
