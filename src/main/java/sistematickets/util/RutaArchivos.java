96b456bdc2e6c2c86a8fd0962012f34ce6172bae
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 11:45:43 -0500

    Feat: creacion de la clase RutaArchivos en util y las rutas de los arichos txt

---
+++ src/main/java/sistematickets/util/RutaArchivos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistematickets.util;
+
+/**
+ *
+ * @author josec
+ */
+public class RutaArchivos {
+    public static final String RUTAS = "rutas.txt";
+    public static final String BUSETAS = "Busetas.txt";
+    public static final String BUSES = "Buses.txt";
+    public static final String MICROBUSES = "Microbuses.txt";
+    public static final String CONDUCTORES = "conductores.txt";  // ← agregar
+    public static final String PASAJEROS = "pasajeros.txt";      // ← agregar
+    public static final String TICKETS = "tickets.txt";          // ← agregar
+}
