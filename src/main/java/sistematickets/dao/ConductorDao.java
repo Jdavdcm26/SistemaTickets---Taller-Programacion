6ea6e1888cbf5a77aba9af72bb486a40d045165d
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-20 13:41:07 -0500

    feat: creacion de los metodos guardarConductores(), cargarConductores() en ConductorDao

--- src/main/java/sistematickets/dao/ConductorDao.java
+++ src/main/java/sistematickets/dao/ConductorDao.java
@@ -4,10 +4,50 @@
  */
 package sistematickets.dao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import sistematickets.model.Conductor;
+import sistematickets.util.RutaArchivos;
+
 /**
  *
  * @author jadis
  */
 public class ConductorDao {
+    
+    public void guardarConductores(HashMap<String, Conductor> conductores) throws IOException {
+        File archivo = new File(RutaArchivos.CONDUCTORES);
+        if (!archivo.exists()) {
+            archivo.createNewFile();
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo))) {
+            for (Conductor c : conductores.values()) {
+                bw.write(c.toFile());
+                bw.newLine();
+            }
+        }
+    }
+
+    public HashMap<String, Conductor> cargarConductores() throws IOException {
+        HashMap<String, Conductor> conductores = new HashMap<>();
+        File archivo = new File(RutaArchivos.CONDUCTORES);
+        if (!archivo.exists()) {
+            return conductores;
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] p = linea.split(";");
+                Conductor c = new Conductor(p[0], p[1], p[2], p[3]);
+                conductores.put(c.getCedula(), c);
+            }
+        }
+        return conductores;
+    }
     
 }
