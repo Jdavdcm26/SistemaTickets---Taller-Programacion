64cd2056c0f4293b00f389eb1f81c5db35a125b5
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 17:43:02 -0500

    Feat: creacion del metodo menu() en PasajeroView

--- src/main/java/sistematickets/view/PasajeroView.java
+++ src/main/java/sistematickets/view/PasajeroView.java
@@ -4,10 +4,39 @@
  */
 package sistematickets.view;
 
+import java.util.Scanner;
+import sistematickets.service.PasajeroService;
+
 /**
  *
  * @author josec
  */
 public class PasajeroView {
-    
+   private Scanner sc = new Scanner(System.in);
+    private PasajeroService pasajeroService = new PasajeroService();
+
+    public void menu() {
+        int opcion;
+        do {
+            System.out.println("\n===== MENÚ PASAJEROS =====");
+            System.out.println("1. Registrar pasajero");
+            System.out.println("2. Listar todos");
+            System.out.println("3. Buscar por cédula");
+            System.out.println("4. Actualizar pasajero");
+            System.out.println("5. Eliminar pasajero");
+            System.out.println("0. Volver");
+            System.out.print("Opción: ");
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1 -> registrar();
+                case 2 -> listarTodos();
+                case 3 -> buscarPorCedula();
+                case 4 -> actualizarPasajero();
+                case 5 -> eliminar();
+                case 0 -> System.out.println("Volviendo...");
+                default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+    }  
 }
