b8a45381800cedb62e68fb952624746c5c09a6c9
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 17:36:22 -0500

    Feat: creacion del metodo menu() en ConductorView

--- src/main/java/sistematickets/view/ConductorView.java
+++ src/main/java/sistematickets/view/ConductorView.java
@@ -4,10 +4,39 @@
  */
 package sistematickets.view;
 
+import java.util.Scanner;
+import sistematickets.service.ConductorService;
+
 /**
  *
  * @author josec
  */
 public class ConductorView {
-    
+  private Scanner sc = new Scanner(System.in);
+    private ConductorService conductorService = new ConductorService();
+
+    public void menu() {
+        int opcion;
+        do {
+            System.out.println("\n===== MENÚ CONDUCTORES =====");
+            System.out.println("1. Registrar conductor");
+            System.out.println("2. Listar todos");
+            System.out.println("3. Buscar por cédula");
+            System.out.println("4. Actualizar conductor");
+            System.out.println("5. Eliminar conductor");
+            System.out.println("0. Volver");
+            System.out.print("Opción: ");
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1 -> registrar();
+                case 2 -> listarTodos();
+                case 3 -> buscarPorCedula();
+                case 4 -> actualizar();
+                case 5 -> eliminar();
+                case 0 -> System.out.println("Volviendo...");
+                default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+    }  
 }
