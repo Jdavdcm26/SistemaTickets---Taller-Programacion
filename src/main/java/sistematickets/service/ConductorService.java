7da54c657860d5e384c4c463843ce71d618c5518
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-20 13:55:22 -0500

    feat: creacion de los metodos agregar(), buscarPorCedula() en ConductorSrvice

--- src/main/java/sistematickets/service/ConductorService.java
+++ src/main/java/sistematickets/service/ConductorService.java
@@ -4,10 +4,39 @@
  */
 package sistematickets.service;
 
+import java.io.IOException;
+import sistematickets.dao.ConductorDao;
+import sistematickets.model.Conductor;
+
 /**
  *
  * @author jadis
  */
 public class ConductorService {
     
+    private ConductorDao conductorDao = new ConductorDao(); 
+    
+    public boolean agregar(Conductor c) {
+        try {
+            // Validación del taller: conductor debe tener licencia
+            if (c.getNumeroLicencia() == null || c.getNumeroLicencia().isBlank()) {
+                return false;
+            }
+            if (conductorDao.buscarPorCedula(c.getCedula()) != null) {
+                return false;
+            }
+            conductorDao.agregarConductor(c);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public Conductor buscarPorCedula(String cedula) {
+        try {
+            return conductorDao.buscarPorCedula(cedula);
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
