6255426cd2847a0e5c90cba18bd2b963ef5a500b
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 17:50:03 -0500

    Feat: creacion del metodo menu() en TicketView

--- src/main/java/sistematickets/view/TicketView.java
+++ src/main/java/sistematickets/view/TicketView.java
@@ -4,10 +4,37 @@
  */
 package sistematickets.view;
 
+import java.util.Scanner;
+import sistematickets.service.TicketService;
+import sistematickets.service.VehiculoService;
+
 /**
  *
  * @author josec
  */
 public class TicketView {
-    
+  private Scanner sc = new Scanner(System.in);
+    private TicketService ticketService   = new TicketService();
+    private VehiculoService vehiculoService = new VehiculoService();
+
+    public void menu() {
+        int opcion;
+        do {
+            System.out.println("\n===== MENÚ TICKETS =====");
+            System.out.println("1. Vender ticket");
+            System.out.println("2. Listar todos");
+            System.out.println("3. Reportes y estadísticas");
+            System.out.println("0. Volver");
+            System.out.print("Opción: ");
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1 -> vender();
+                case 2 -> listarTodos();
+                case 3 -> menuReportes();
+                case 0 -> System.out.println("Volviendo...");
+                default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+    }  
 }
