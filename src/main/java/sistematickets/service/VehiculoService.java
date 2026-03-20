1d22b6c3cd7e54619d0b4a5b1715cf0c7ab45039
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 15:09:45 -0500

    Feat: creacion de los metodos agregar(), buscarPorPlaca(), ListarTodos() en VehiculoService

--- src/main/java/sistematickets/service/VehiculoService.java
+++ src/main/java/sistematickets/service/VehiculoService.java
@@ -4,10 +4,47 @@
  */
 package sistematickets.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import sistematickets.dao.ConductorDao;
+import sistematickets.dao.RutaDao;
+import sistematickets.dao.VehiculoDao;
+import sistematickets.model.Vehiculo;
+
 /**
  *
  * @author josec
  */
 public class VehiculoService {
-    
+    private VehiculoDao vehiculoDao  = new VehiculoDao();
+    private RutaDao rutaDao          = new RutaDao();
+    private ConductorDao conductorDao = new ConductorDao();
+
+    public boolean agregar(Vehiculo v) {
+        try {
+            if (vehiculoDao.buscarPorPlaca(v.getPlaca(), rutaDao, conductorDao) != null) {
+                return false; // placa duplicada
+            }
+            vehiculoDao.agregarVehiculo(v);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public Vehiculo buscarPorPlaca(String placa) {
+        try {
+            return vehiculoDao.buscarPorPlaca(placa, rutaDao, conductorDao);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public ArrayList<Vehiculo> listarTodos() {
+        try {
+            return vehiculoDao.cargarLista(rutaDao, conductorDao);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
 }
