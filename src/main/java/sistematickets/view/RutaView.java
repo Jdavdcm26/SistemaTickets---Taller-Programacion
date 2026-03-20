67fc2a7fec7f1d619f392dc2bbfd1a1c298493ce
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 17:01:15 -0500

    Feat: creacion del metodo menu() en RutaView

--- src/main/java/sistematickets/view/RutaView.java
+++ src/main/java/sistematickets/view/RutaView.java
@@ -4,10 +4,39 @@
  */
 package sistematickets.view;
 
+import java.util.Scanner;
+import sistematickets.service.RutaService;
+
 /**
  *
  * @author josec
  */
 public class RutaView {
-    
+    private Scanner sc = new Scanner(System.in);
+    private RutaService rutaService = new RutaService();
+ 
+    public void menu() {
+        int opcion;
+        do {
+            System.out.println("\n===== MENÚ RUTAS =====");
+            System.out.println("1. Agregar ruta");
+            System.out.println("2. Listar todas");
+            System.out.println("3. Buscar por código");
+            System.out.println("4. Actualizar ruta");
+            System.out.println("5. Eliminar ruta");
+            System.out.println("0. Volver");
+            System.out.print("Opción: ");
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1 -> agregar();
+                case 2 -> listarTodas();
+                case 3 -> buscarPorCodigo();
+                case 4 -> actualizar();
+                case 5 -> eliminar();
+                case 0 -> System.out.println("Volviendo...");
+                default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+    }
 }
