fef439eecc412bc39ec4ad595bc299e0b7aad7b0
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-20 11:15:29 -0500

    Feat: creacion de la clase Ruta y sus atributos

---
+++ src/main/java/sistematickets/model/Ruta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistematickets.model;
+
+/**
+ *
+ * @author josec
+ */
+public class Ruta implements InterfazImprimible {
+    private String codRuta;
+    private String ciuOrigen;
+    private String ciuDestino;
+    private int kilometros;
+    private int tiemReco;
+}
