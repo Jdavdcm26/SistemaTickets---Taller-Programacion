0d9785ce4e6c2c188be5e27ad096f7c1b8aae2d9
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-21 10:43:09 -0500

    feat: agregar encapsulamiento, getter y setter a clase Reserva

--- src/main/java/sistematickets/model/Reserva.java
+++ src/main/java/sistematickets/model/Reserva.java
@@ -22,4 +22,53 @@ public class Reserva implements InterfazImprimible{
     private LocalDate fechaViaje;
     private Estado estado;
     
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo, LocalDate fechaViaje) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaCreacion = LocalDateTime.now();
+        this.fechaViaje = fechaViaje;
+        this.estado = Estado.ACTIVA;
+    }
+
+    
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo,
+        LocalDateTime fechaCreacion, LocalDate fechaViaje, Estado estado) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaViaje = fechaViaje;
+        this.estado = estado;
+    }
+    
+    public String getCodigo() { 
+        return codigo; 
+    }
+    
+    public Pasajero getPasajero() { 
+        return pasajero; 
+    }
+    
+    public Vehiculo getVehiculo() { 
+        return vehiculo; 
+    }
+    
+    public LocalDateTime getFechaCreacion() { 
+        return fechaCreacion; 
+    }
+    
+    public LocalDate getFechaViaje() { 
+        return fechaViaje; 
+    }
+    
+    public Estado getEstado() { 
+        return estado; 
+    }
+    
+    public void setEstado(Estado estado) { 
+        this.estado = estado; 
+    }
+    
+    
 }
