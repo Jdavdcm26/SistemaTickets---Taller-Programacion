e78b6cd62ad6bc79b2569f0fa908353fa5b97c34
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-21 14:35:43 -0500

    fix: agregrar throws IO exception en el main

--- src/main/java/sistematickets/sistematickets/SistemaTickets.java
+++ src/main/java/sistematickets/sistematickets/SistemaTickets.java
@@ -1,10 +1,11 @@
 package sistematickets.sistematickets;
 
+import java.io.IOException;
 import sistematickets.view.MenuPrincipal;
 
 public class SistemaTickets {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MenuPrincipal menu = new MenuPrincipal();
         menu.iniciar();
     }
