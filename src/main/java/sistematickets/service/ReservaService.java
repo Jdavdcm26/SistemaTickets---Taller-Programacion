b4129e20435b32bcd6fbf733f9f8ba3b79c4e7dd
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-03-21 12:09:35 -0500

    feat: agregar dependencias DAO y TicketService en ReservaService

--- src/main/java/sistematickets/service/ReservaService.java
+++ src/main/java/sistematickets/service/ReservaService.java
@@ -4,10 +4,23 @@
  */
 package sistematickets.service;
 
+import sistematickets.dao.ConductorDao;
+import sistematickets.dao.PasajeroDao;
+import sistematickets.dao.ReservaDao;
+import sistematickets.dao.RutaDao;
+import sistematickets.dao.VehiculoDao;
+
 /**
  *
  * @author jadis
  */
 public class ReservaService {
+    
+    private ReservaDao reservaDao     = new ReservaDao();
+    private PasajeroDao pasajeroDao   = new PasajeroDao();
+    private VehiculoDao vehiculoDao   = new VehiculoDao();
+    private RutaDao rutaDao           = new RutaDao();
+    private ConductorDao conductorDao = new ConductorDao();
+    private TicketService ticketService = new TicketService();
     
 }
